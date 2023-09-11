8dceec6659e809618ff583dca70235037a29e471
FabioPinheiro <fabiomgpinheiro@gmail.com>
2023-09-11 18:36:23 +0100

    Add the agent iohkMediatorBeta

--- did-example/shared/src/main/scala/fmgp/did/AgentProvider.scala
+++ did-example/shared/src/main/scala/fmgp/did/AgentProvider.scala
@@ -98,7 +98,7 @@ object AgentProvider {
       AgentWithShortName("ivan", ivan),
       AgentWithShortName("pat", pat),
       AgentWithShortName("victor", victor),
-      AgentWithShortName("iohkMediator", iohkMediator),
+      AgentWithShortName("iohkMediatorBeta", iohkMediatorBeta),
       AgentWithShortName("iohkMediatorSit", iohkMediatorSit),
       AgentWithShortName("exampleAlice", exampleAlice),
       AgentWithShortName("exampleBob", exampleBob),
@@ -233,12 +233,12 @@ object AgentProvider {
     Seq(DIDPeerServiceEncoded(s = "http://localhost:8080"))
   )
 
-  val iohkMediator = DIDPeer2.makeAgent(
+  val iohkMediatorBeta = DIDPeer2.makeAgent(
     Seq(
       keyAgreement("Z6D8LduZgZ6LnrOHPrMTS6uU2u5Btsrk1SGs4fn8M7c", "Sr4SkIskjN_VdKTn0zkjYbhGTWArdUNE4j_DmUpnQGw"),
       keyAuthentication("INXCnxFEl0atLIIQYruHzGd5sUivMRyQOzu87qVerug", "MBjnXZxkMcoQVVL21hahWAw43RuAG-i64ipbeKKqwoA")
     ),
-    Seq(DIDPeerServiceEncoded(s = "https://mediator-test-env.atalaprism.io/mediator"))
+    Seq(DIDPeerServiceEncoded(s = "https://beta-mediator.atalaprism.io"))
   )
 
   val iohkMediatorSit = DIDPeer2.makeAgent(
