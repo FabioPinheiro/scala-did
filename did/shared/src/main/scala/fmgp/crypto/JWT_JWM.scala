5708699a3a4a486c0bc801099fcd85c80a4cbc0a
FabioPinheiro <fabiomgpinheiro@gmail.com>
2024-05-29 00:49:03 +0100

    Remake JWTPayload & JWT's utils methods in Attachment

--- did/shared/src/main/scala/fmgp/crypto/JWT_JWM.scala
+++ did/shared/src/main/scala/fmgp/crypto/JWT_JWM.scala
@@ -6,6 +6,9 @@ import fmgp.crypto.SHA256
 import fmgp.did.VerificationMethodReferenced
 import fmgp.did.SetU
 import fmgp.did.comm.KWAlgorithm
+import scala.util.Try
+import scala.util.Failure
+import scala.util.Success
 
 // ###########
 // ### JWT ###
@@ -35,28 +38,37 @@ object SignatureJWM:
 
 opaque type JWT = (ProtectedHeaderJWT, Payload, SignatureJWT)
 object JWT {
-
   def apply(protectedHeader: ProtectedHeaderJWT, payload: Payload, signature: SignatureJWT): JWT =
     (protectedHeader, payload, signature)
-
   def fromStrings(protectedHeader: String, payload: String, signature: String): JWT =
     apply(
       ProtectedHeaderJWT.fromBase64url(protectedHeader),
       Payload.fromBase64url(payload),
       SignatureJWT.fromBase64url(signature)
     )
-
   def unsafeFromEncodedJWT(str: String): JWT = str.split('.') match {
     case Array(protectedHeader: String, payload: String, signature: String) =>
       fromStrings(protectedHeader, payload, signature)
   }
+
+  /** This is the JWT in base64JWTFormat encode in Base64 (double encoded) */
+  def unsafeFromBase64(base64: Base64): JWT = unsafeFromEncodedJWT(base64.decodeToString)
+
+  /** This is the JWT in base64JWTFormat encode in Base64 (double encoded) */
+  def fromBase64(base64: Base64): Either[String, JWT] =
+    Try(unsafeFromEncodedJWT(base64.decodeToString)) match // TODO improve
+      case Failure(exception) => Left(s"Fail fromBase64 due: $exception")
+      case Success(value)     => Right(value)
+
   extension (jwt: JWT)
     inline def protectedHeader: ProtectedHeaderJWT = jwt._1
     inline def payload: Payload = jwt._2
     inline def signature: SignatureJWT = jwt._3
     def base64JWTFormatWithNoSignature = jwt.protectedHeader.urlBase64 + "." + jwt.payload.urlBase64
     def base64JWTFormat = jwt.base64JWTFormatWithNoSignature + "." + jwt.signature.urlBase64
-    // def base64: Base64 = Base64.fromBase64url(jwt.value)
+
+    /** This is the JWT in base64JWTFormat encode in Base64 (double encoded) */
+    def base64: Base64 = Base64.encode(jwt.base64JWTFormat)
 }
 
 opaque type ProtectedHeaderJWT = Base64
@@ -107,20 +119,29 @@ object JWTHeader {
   given encoder: JsonEncoder[JWTHeader] = DeriveJsonEncoder.gen[JWTHeader]
 }
 
-/** @see
-  *   based on https://github.com/panva/jose/blob/main/docs/interfaces/types.JWTPayload.md
+opaque type JWTPayload = ast.Json.Obj
+
+/** JWT's Payload
+  * @see
+  *   https://github.com/panva/jose/blob/main/docs/interfaces/types.JWTPayload.md
   */
-final case class JWTPayload(
-    aud: Option[SetU[String]],
-    exp: Option[Long],
-    iat: Option[Long],
-    iss: Option[String],
-    jti: Option[String],
-    nbf: Option[Long],
-    sub: Option[String],
-)
 object JWTPayload {
-  import fmgp.did.SetU.given
-  given decoder: JsonDecoder[JWTPayload] = DeriveJsonDecoder.gen[JWTPayload]
-  given encoder: JsonEncoder[JWTPayload] = DeriveJsonEncoder.gen[JWTPayload]
+  def apply(obj: ast.Json.Obj): JWTPayload = obj
+  extension (obj: JWTPayload)
+    def json: ast.Json.Obj = obj
+
+    /** Issuer https://datatracker.ietf.org/doc/html/rfc7519#section-4.1.1 */
+    def getISS: Option[String] = obj.get("iss").flatMap(_.asString)
+
+    /** Subject https://datatracker.ietf.org/doc/html/rfc7519#section-4.1.2 */
+    def getSUB: Option[String] = obj.get("sub").flatMap(_.asString)
+
+    /** Audience https://datatracker.ietf.org/doc/html/rfc7519#section-4.1.3 */
+    def getAUD: Option[String] = obj.get("aud").flatMap(_.asString)
+
+    /** Expiration Time https://datatracker.ietf.org/doc/html/rfc7519#section-4.1.4 */
+    def getEXP: Option[String] = obj.get("exp").flatMap(_.asString)
+
+  given decoder: JsonDecoder[JWTPayload] = ast.Json.Obj.decoder
+  given encoder: JsonEncoder[JWTPayload] = ast.Json.Obj.encoder
 }
