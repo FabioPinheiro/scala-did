037c5ead9e4988a759bdad3351904377f8c5c568
FabioPinheiro <fabiomgpinheiro@gmail.com>
2025-07-08 00:53:47 +0100

    did-method-prism: make mnemonics more type safe

--- did-method-prism/shared/src/main/scala/fmgp/did/method/prism/cardano/CardanoWalletConfig.scala
+++ did-method-prism/shared/src/main/scala/fmgp/did/method/prism/cardano/CardanoWalletConfig.scala
@@ -32,3 +32,43 @@ case class CardanoWalletConfig(
 ) {
   def mnemonicPhrase = mnemonic.mkString(" ")
 }
+
+object CardanoWalletConfig {
+  def fromMnemonicPhrase(phrase: String, passphrase: String = "") = phrase.split(" ") match
+    case words @ Array(
+          w01,
+          w02,
+          w03,
+          w04,
+          w05,
+          w06,
+          w07,
+          w08,
+          w09,
+          w10,
+          w11,
+          w12,
+          w13,
+          w14,
+          w15,
+          w16,
+          w17,
+          w18,
+          w19,
+          w20,
+          w21,
+          w22,
+          w23,
+          w24
+        ) =>
+      // Check words https://github.com/cardano-foundation/cardano-wallet/blob/master/specifications/mnemonic/english.txt
+      if (words.exists(w => MnemonicEnglish.words.contains(w)))
+        // TODO check Checksums https://cips.cardano.org/cip/CIP-4
+        Right(CardanoWalletConfig(words.toSeq, passphrase))
+      else
+        Left(
+          "MnemonicPhrase MUST online contains words from 'https://github.com/cardano-foundation/cardano-wallet/blob/master/specifications/mnemonic/english.txt'"
+        )
+    case _ => Left("MnemonicPhrase MSU have 24 words")
+
+}
