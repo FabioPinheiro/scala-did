b01fd81b6ec82abb286905ac2ec1a0a71056ce0c
FabioPinheiro <fabiomgpinheiro@gmail.com>
2026-02-25 22:30:49 +0000

    did-method-prism: support all CardanoNetwork in the BlockfrostConfig extension method nodeProvider

--- did-method-prism/jvm/src/main/scala/fmgp/did/method/prism/BlockfrostConfigExtension.scala
+++ did-method-prism/jvm/src/main/scala/fmgp/did/method/prism/BlockfrostConfigExtension.scala
@@ -7,7 +7,7 @@ import fmgp.did.method.prism.cardano.*
 extension (bf: BlockfrostConfig)
   def nodeProvider: Task[BlockfrostProvider] = bf.network match
     case PublicCardanoNetwork.Mainnet => ZIO.fromFuture(implicit ec => BlockfrostProvider.mainnet(bf.token))
-    case PublicCardanoNetwork.Testnet => ???
-    case PublicCardanoNetwork.Preprod => ??? // TODO
-    case PublicCardanoNetwork.Preview => ??? // TODO
+    case PublicCardanoNetwork.Testnet => ZIO.fail(RuntimeException("Cardano 'testnet' does not exist anymore"))
+    case PublicCardanoNetwork.Preprod => ZIO.fromFuture(implicit ec => BlockfrostProvider.preprod(bf.token))
+    case PublicCardanoNetwork.Preview => ZIO.fromFuture(implicit ec => BlockfrostProvider.preview(bf.token))
     case PrivateCardanoNetwork(blockfrostURL, protocolMagic) => ???
