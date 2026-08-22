24622de5fa65114a2d0c214a7a2b933502da81fd
FabioPinheiro <fabiomgpinheiro@gmail.com>
2026-08-23 00:06:42 +0100

    Update cardano-client-backend-blockfrost, ... to 0.8.0-pre4

--- did-method-prism/jvm/src/main/scala/fmgp/did/method/prism/CardanoService.scala
+++ did-method-prism/jvm/src/main/scala/fmgp/did/method/prism/CardanoService.scala
@@ -161,11 +161,11 @@ object CardanoService {
   private def makeBFBackendService(bfConfig: BlockfrostConfig) =
     new BFBackendService(bfConfig.network.blockfrostURL + "/", bfConfig.token)
   @deprecated("use ScalusService object instead")
-  @annotation.nowarn
+  @ _root_.scala.annotation.nowarn
   def makeAccount(bfConfig: BlockfrostConfig, wallet: CardanoWalletConfig): Account =
     new Account(makeBFNetworks(bfConfig.network), wallet.mnemonicPhrase)
   @deprecated("use ScalusService object instead")
-  @annotation.nowarn
+  @ _root_.scala.annotation.nowarn
   def makeAccount(network: PublicCardanoNetwork, wallet: CardanoWalletConfig): Account =
     new Account(makeBFNetworks(network), wallet.mnemonicPhrase)
 
